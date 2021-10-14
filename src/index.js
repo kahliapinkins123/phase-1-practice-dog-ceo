b6b4022056d3fd6c019eb2bf3d01f045ac3b235c
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-14 13:24:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl).then(resp => resp.json())
+.then(function(json){
+    json.message.forEach(function(dogUrl){
+        dogImg = document.createElement('img')
+        dogImg.src = dogUrl;
+        console.log(dogImg)
+        document.querySelector('#dog-breeds').appendChild(dogImg);
+    })
+    
+})
