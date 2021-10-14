26a2b67e91de36f12ad748a29284d0eb2bcd8ea6
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-14 14:11:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,40 +3,30 @@ console.log('%c HI', 'color: firebrick')
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    let dogBreeds = document.querySelector('#dog-breeds');
 
-fetch(imgUrl).then(resp => resp.json())
-.then(function(json){
-    json.message.forEach(function(dogUrl){
-        let dogImg = document.createElement('img')
-        dogImg.src = dogUrl;
-        console.log(dogImg)
-        document.querySelector('#dog-breeds').appendChild(dogImg);
-    })
+
+    fetch(imgUrl).then(resp => resp.json())
+    .then(function(json){
+        json.message.forEach(function(dogUrl){
+            let dogImg = document.createElement('img')
+            dogImg.src = dogUrl;
+            console.log(dogImg)
+            dogBreeds.appendChild(dogImg);
+        })
     
-})
+    })
 
-/*fetch(breedUrl).then(resp=>resp.json())
-.then(function(json){
-
-})
+    fetch(breedUrl).then(resp=>resp.json())
+    .then(function(json){
+        console.log(json)
+        for (const dogBreed in json.message){
+            let li = document.createElement('li');
+            li.innerText = dogBreed;
+            dogBreeds.appendChild(li);
+        }
+    })
+        
 });
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
-fetch(imgUrl).then(resp => resp.json())
-.then(function(json){
-    json.message.forEach(function(dogUrl){
-        let dogImg = document.createElement('img')
-        dogImg.src = dogUrl;
-        console.log(dogImg)
-        document.querySelector('#dog-breeds').appendChild(dogImg);
-    })
-    
-})
-
-fetch(breedUrl).then(resp=>resp.json())
-.then(function(json){
-
-})*/
