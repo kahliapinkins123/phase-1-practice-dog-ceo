bea6790327e6b0b62b5790b89f9918b6df47477d
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-14 15:44:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
     let dogBreeds = document.querySelector('#dog-breeds');
+    let dogImgContainer = document.querySelector('#dog-image-container');
+    let dropdown = document.querySelectorAll('#breed-dropdown option');
 
 
+    console.log(dropdown)
     fetch(imgUrl).then(resp => resp.json())
     .then(function(json){
         json.message.forEach(function(dogUrl){
             let dogImg = document.createElement('img')
             dogImg.src = dogUrl;
-            console.log(dogImg)
-            dogBreeds.appendChild(dogImg);
+            dogImgContainer.appendChild(dogImg);
         })
     
     })
 
     fetch(breedUrl).then(resp=>resp.json())
     .then(function(json){
-        console.log(json)
         for (const dogBreed in json.message){
             let li = document.createElement('li');
             li.innerText = dogBreed;
             dogBreeds.appendChild(li);
+
+            li.addEventListener('click', () => {
+                li.style.color = 'blue';
+            });
         }
+    })
+
+    dropdown.forEach(function(option){
+        console.log(option)
+        option.addEventListener('click', (e)=> console.log(e))
     })
         
 });
-
