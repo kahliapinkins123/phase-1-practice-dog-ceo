f189bb7eb168edecd0fc6cd2f44cedc64748e403
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-14 13:44:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ console.log('%c HI', 'color: firebrick')
 
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(imgUrl).then(resp => resp.json())
+.then(function(json){
+    json.message.forEach(function(dogUrl){
+        let dogImg = document.createElement('img')
+        dogImg.src = dogUrl;
+        console.log(dogImg)
+        document.querySelector('#dog-breeds').appendChild(dogImg);
+    })
+    
+})
+
+/*fetch(breedUrl).then(resp=>resp.json())
+.then(function(json){
+
+})
 });
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
@@ -21,4 +39,4 @@ fetch(imgUrl).then(resp => resp.json())
 fetch(breedUrl).then(resp=>resp.json())
 .then(function(json){
 
-})
+})*/
